15b7caa350a7908e5143debc9338eb29200f1dc9
Wiley <wiley@nurigo.net>
2017-10-19 15:43:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello Spinnaker!!');
+  res.send('Hello Spinnaker!');
 });
 
 app.get('/healthcheck', function (req, res) {
